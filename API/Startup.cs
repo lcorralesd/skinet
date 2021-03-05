44909508816d7c98c2714f4f93d3d5bacc684cc4
Libardo Corrales D <lcorralesd@gmail.com>
2021-03-05 10:14:27 -0500

    Change database, and add remove-migrations and add new migrations with sqlserver

--- API/Startup.cs
+++ API/Startup.cs
@@ -33,8 +33,8 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddDbContext<StoreContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("default")));
-            services.AddDbContext<StoreContext>(opt => opt.UseSqlite(Configuration.GetConnectionString("sqlite")));
+            services.AddDbContext<StoreContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("default")));
+            //services.AddDbContext<StoreContext>(opt => opt.UseSqlite(Configuration.GetConnectionString("sqlite")));
             services.AddControllers();
             services.AddSwaggerDocumentation();
 
